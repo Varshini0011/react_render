de63aa93dca1a8f9143f74a5f7210d12832add21
Varshini0011 <149299028+Varshini0011@users.noreply.github.com>
2026-03-06 11:05:32 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://cabsystemsms-1.onrender.com/register",
+        "https://project-render-y4ma.onrender.com/register",
         data
       )
          alert(res.data)
